8fc73f3f3aa9a6ce3ea8a17badeb65f85996546b
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2016-04-27 21:33:30 -0400

    Add CMakesLists and change gain parameters for the controller

--- src/feedback_controller/src/controller.py
+++ src/feedback_controller/src/controller.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 
+from __future__ import division
 import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Point
-
 
 def controller_callback(error):
     global sumError_x
     global sumError_y
     command = Twist()
     Kp_x = 1/640
-    Ki_x = 1/1000
+    Ki_x = 1/100000
     Kp_y = 1/360
-    Ki_y = 1/1000
+    Ki_y = 1/100000
 
     sumError_x = sumError_x + error.x
     sumError_y = sumError_y + error.y
     command.linear.x = Kp_x * error.x + Ki_x * sumError_x
     command.linear.y = Kp_y * error.y + Ki_y * sumError_y
-    control_publisher(command)
-    print command
+    control_publisher.publish(command)
+    # print error
+    # print sumError_y, sumError_x
+    # print command
+    # print "*************************"
 
 if __name__ == "__main__":
     rospy.init_node('controller')
@@ -27,3 +30,4 @@ if __name__ == "__main__":
     control_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     sumError_x = 0
     sumError_y = 0
+    rospy.spin()
